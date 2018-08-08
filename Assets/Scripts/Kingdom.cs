74671cfbf0522049b79634f82259694a15968161
Ignacy <ignas_ignacy@o2.pl>
2018-08-08 21:24:28 +0200

    Kingdom class and the beginning of StaticValues static class
    Added Kingdom class. Added StaticValues static class with one field at the moment. Whenever you need to initialize a field with a set value, add a field to StaticValues and use that.

---
+++ Assets/Scripts/Kingdom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Kingdom
+{
+    public string Name { get; private set; }
+    public int ID { get; private set; }
+    public int Chaos { get; set; }
+    public string Description { get; private set; }
+
+    public Kingdom(string name, int id, string description)
+    {
+        Name = name;
+        ID = id;
+        Chaos = StaticValues.startingChaos;
+        Description = description;
+    }
+}
