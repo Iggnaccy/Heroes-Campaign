46e380e9818ec5ee9ec1b67c27d24b2aa8cf0b84
stawrocek <stasiukoza1234@gmail.com>
2018-08-11 20:39:56 +0200

    Hero

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Hero {
+    public enum HeroProfession {
+        Archer,
+        Druid,
+        Intern,
+        Knight,
+        Mage,
+        Rogue,
+        Warrior
+    };
+    public int Exp { get; set; }
+    public int Level { get; set; }
+    public string Name { get; private set; }
+    public HeroProfession Profession { get; private set; }
+    public int Salary { get; set; }
+
+    public Hero(string name, HeroProfession profession, int exp, int level, int salary) {
+        Name = name;
+        Profession = profession;
+        Exp = exp;
+        Level = level;
+        Salary = salary;
+    }
+}
