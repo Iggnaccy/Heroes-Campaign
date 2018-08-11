9da5386cbb068ef327411d73ed9546a3a9381d82
Ignacy <ignas_ignacy@o2.pl>
2018-08-12 01:21:52 +0200

    Completed tabs
    Completed tabs, working buttons, created prefabs for spawning buttons in tabs (like a button for each hero, or for each mission available). Further development needs classes from other issues

--- Assets/Scripts/ExitButtonScript.cs
+++ Assets/Scripts/ExitButtonScript.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ExitButtonScript : MonoBehaviour
 {
     public void Click()
     {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
         Application.Quit();
+        #endif
     }
 }
