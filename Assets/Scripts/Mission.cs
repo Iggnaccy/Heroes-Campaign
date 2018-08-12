5cb48953cb95c1bf0d611d9cb70584cee2e0ae74
gromdd <incognito1@o2.pl>
2018-08-12 16:52:42 +0200

    first implementation of mission
    REEEEEEEEEEEEEEEE

---
+++ Assets/Scripts/Mission.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mission
+{
+    public enum MissionTypes
+    {
+        Exploration,
+        Escort,
+        Extermination,
+        Defence
+    }
+
+
+
+
+
+    public Kingdom AreaOfMission { get; private set; }
+    public Hero ParticipatingHero { get; private set; }
+    public Player PiggyBank { get; private set; }
+
+
+
+    public int ChaosReduction { get; private set; }
+    public int GoldEarned { get; private set; }
+    public int ExpEarned { get; private set; }
+    public int FameEarned { get; private set; }
+    public MissionTypes MissionType { get; private set; }
+
+
+
+    public Mission(ref Kingdom areaOfMission, ref Hero participatingHero, ref Player piggyBank, int chaosReduction, int goldEarned, int expEarned, int fameEarned, MissionTypes missionType)
+    {
+        AreaOfMission = areaOfMission;
+        ParticipatingHero = participatingHero;
+        PiggyBank = piggyBank;
+
+        ChaosReduction = chaosReduction;
+        GoldEarned = goldEarned;
+        ExpEarned = expEarned;
+        FameEarned = fameEarned;
+        MissionType = missionType;
+    }
+
+
+    public virtual void BeginMission()
+    {
+
+
+    }
+
+    public virtual void Victory()
+    {
+        if (ParticipatingHero.Level < StaticValues.LevelCap)
+        {
+            ParticipatingHero.Exp += ExpEarned;
+            while (ParticipatingHero.Exp > StaticValues.ExpNeededToNextLevel[ParticipatingHero.Level])
+            {
+                ParticipatingHero.Exp -= StaticValues.ExpNeededToNextLevel[ParticipatingHero.Level];
+                ParticipatingHero.Level++;
+                if (ParticipatingHero.Level == StaticValues.LevelCap)
+                {
+                    ParticipatingHero.Exp = 0;
+                    break;
+                }
+            }
+        }
+
+
+        PiggyBank.Fame += FameEarned;
+        PiggyBank.Gold += GoldEarned;
+
+
+        AreaOfMission.Chaos = System.Math.Max(0, AreaOfMission.Chaos - ChaosReduction);
+    }
+
+
+
+
+    public virtual void Defeat()
+    {
+
+    }
+
+
+
+}
--- Assets/Scripts/StaticValues.cs
+++ Assets/Scripts/StaticValues.cs
@@ -6,4 +6,6 @@ public static class StaticValues
     public static int startingChaos = 25;
     public static int startingExp = 0;
     public static int startingLevel = 0;
+    public static int LevelCap = 15;
+    public static int[] ExpNeededToNextLevel = { 100, 200, 300, 500, 700, 1000, 1500, 2000, 3000, 4500, 7000, 10000, 15000, 25000, 45000 };
 }
