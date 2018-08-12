066fc071d63125ecd3048e715787a87cc75a26d9
stawrocek <stasiukoza1234@gmail.com>
2018-08-12 13:43:30 +0200

    Refactor #2
    git haven't realized that I changed this file too...

--- Assets/Scripts/StaticValues.cs
+++ Assets/Scripts/StaticValues.cs
@@ -4,4 +4,6 @@ using System.Collections.Generic;
 public static class StaticValues
 {
     public static int startingChaos = 25;
+    public static int startingExp = 0;
+    public static int startingLevel = 0;
 }
