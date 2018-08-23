0c74ddccb920471e9a9d4869fbe602024aebc2c6
lukaszWroc <38118395+lukaszWroc@users.noreply.github.com>
2018-08-23 20:38:52 +0200

    cleaning2

---
+++ Assets/Scripts/Event/GoldWasFoundArgs.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldWasFoundArgs{
+
+
+    Player player;
+    public GoldWasFoundArgs(Player _player)
+    {
+        player = _player;
+    }
+
+
+}
---
+++ Assets/Scripts/Event/SpecialMissionArgs.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialMissionArgs{
+
+    Player player;
+	public SpecialMissionArgs(Player _player)
+    {
+        player = _player;
+    }
+	
+}
