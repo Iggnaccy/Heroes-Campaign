cfb9d7271b3dcee865bce92ed31cfff5a79e7986
lukaszWroc <galaslukasz1998.com>
2018-08-24 16:15:37 +0200

    final fix fixing is now merged with game-over

--- Assets/Scripts/Kingdom.cs
+++ Assets/Scripts/Kingdom.cs
@@ -7,12 +7,24 @@ public class Kingdom
     public int ID { get; private set; }
     public int Chaos { get; set; }
     public string Description { get; private set; }
-
+    public static int ChaosOverwhelming { get;  set; } = 0;
+  
+    public bool alive;
     public Kingdom(string name, int id, string description)
     {
+        alive = true;
         Name = name;
         ID = id;
         Chaos = StaticValues.startingChaos;
         Description = description;
     }
+
+    public void CheckIfAlive()
+    {
+       if (Chaos < StaticValues.MaxChaos)
+            return;
+        alive = false;
+        ChaosOverwhelming++;
+    }
+
 }
